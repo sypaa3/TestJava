b0acd2fcb57e6827a1ecbb8fe877848fc6c520c6
sypaa3 <maciexxx@gmail.com>
2018-11-20 00:05:44 +0100

    dodalem kilka funkcji

--- SandBox/src/main/java/pl/stqa/pft/sandbox/Test.java
+++ SandBox/src/main/java/pl/stqa/pft/sandbox/Test.java
@@ -3,7 +3,31 @@ package pl.stqa.pft.sandbox;
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("world");
+    hello("user");
+    hello("Maciej");
+
+    double l = 5;
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Pole kwadratu o boku " + l + " = " + area(l));
+    System.out.println("Pole prostokata o bokach " + a + " i " + b  + " = " + area(a,b));
+
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double len) {
+    return len * len;
+
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+
+  }
 }
