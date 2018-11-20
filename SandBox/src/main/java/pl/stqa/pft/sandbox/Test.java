99645f2dbda77cc440c7cd287785b07ebaba0f49
sypaa3 <45147912+sypaa3@users.noreply.github.com>
2018-11-20 12:07:34 +0100

    Adding Ex 2

--- SandBox/src/main/java/pl/stqa/pft/sandbox/Test.java
+++ SandBox/src/main/java/pl/stqa/pft/sandbox/Test.java
@@ -14,7 +14,14 @@ public class Test {
     System.out.println("Pole kwadratu o boku " + l + " = " + area(l));
     System.out.println("Pole prostokata o bokach " + a + " i " + b  + " = " + area(a,b));
 
+    Point p1 = new Point(2.0, 3.5);
+    Point p2 = new Point(8.2, 2.7);
+
+    System.out.println("Distance between P1 and P2 = " + Point.distance(p1, p2));
+    System.out.println("Distance between P1 and P2 = (method using)"+ p1.distance(p2));
   }
+
+
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
